88d2b66d4a83a7d279ef392a0de6e044ff7e83c7
zhengaofen777 <zhnegaofeng@163.com>
2020-03-13 20:20:58 +0800

    Initial commit

---
+++ src/com/zgf/test/hello.java
@@ -0,0 +1,11 @@
+package com.zgf.test;
+
+/**
+ * @author shkstart
+ * @create 2020-03-13 20:18 Administrator
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello  World!");
+    }
+}
